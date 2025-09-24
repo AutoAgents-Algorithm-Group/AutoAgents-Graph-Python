1a8d20bc2046c67c1c36add2f33621bf39bfa84b
sohyan527 <1559463361@qq.com>
2025-09-24 16:15:22 +0800

    update

--- src/autoagents_graph/agentify/__init__.py
+++ src/autoagents_graph/agentify/__init__.py
@@ -1,9 +1,7 @@
-# src/agentify/__init__.py - Agentify 模块
+
 from .FlowGraph import FlowGraph, START
 from .NodeRegistry import NODE_TEMPLATES
-# from .AutoWorkFlow import AutoWorkFlow  # AutoWorkFlow文件不存在
 from .FlowInterpreter import FlowInterpreter
-# from .Text2Workflow import Text2Workflow  # Text2Workflow已移动到src根目录
 from .Utils import (
     StateConverter, NodeValidator, NodeBuilder, EdgeValidator, GraphProcessor,
     DataConverter, TemplateProcessor
